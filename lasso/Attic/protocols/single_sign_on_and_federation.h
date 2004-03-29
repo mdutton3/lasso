74a963be6a69c61c94beb8c827636d99c789272b
Nicolas Clapies <nclapies@entrouvert.com>
2004-03-29 13:01:35 +0000

    add lasso_build_authnRequest method

--- lasso/Attic/protocols/single_sign_on_and_federation.h
+++ lasso/Attic/protocols/single_sign_on_and_federation.h
@@ -3,18 +3,19 @@
 
 #include <lasso/lasso.h>
 
-xmlChar *lasso_build_url_encoded_message_authnRequest(const char *,
-													  const char *,
-													  const char *,
-													  const char *,
-													  const char *,
-													  const char *,
-													  const char **,
-													  const char **,
-													  const char *,
-													  const char *,
-													  const char *,
-													  const char **,
-													  const char *);
+xmlChar *lasso_build_url_encoded_message_authnRequest(LassoNode *);
+
+LassoNode *lasso_build_authnRequest(const char *providerID,
+									const char *nameIDPolicy,
+									const char *isPassive,
+									const char *forceAuthn,
+									const char *assertionConsumerServiceID,
+									const char **authnContextClassRefs,
+									const char **authnContextStatementRefs,
+									const char *authnContextComparison,
+									const char *relayState,
+									const char *proxyCount,
+									const char **idpList,
+									const char *consent);
 
 #endif
