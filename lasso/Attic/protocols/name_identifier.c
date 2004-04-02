9d3578f8a1a850c615f5354e5f8f04823b2bfadb
Nicolas Clapies <nclapies@entrouvert.com>
2004-04-02 16:40:47 +0000

    initial version

---
+++ lasso/Attic/protocols/name_identifier.c
@@ -0,0 +1,53 @@
+#include <lasso/protocols/name_identifier.h>
+
+LassoNode *lasso_build_nameIdentifier(const xmlChar *nameIdentifier,
+				      const xmlChar *nameQualifier,
+				      const xmlChar *format)
+{
+     LassoNode *identifier;
+
+     identifier = lasso_saml_name_identifier_new();
+     lasso_saml_name_identifier_set_nameQualifier(identifier, nameQualifier);
+     lasso_saml_name_identifier_set_format(identifier, format);
+
+     return(identifier);
+}
+
+LassoNode *lasso_build_idpProvidedNameIdentifier(const xmlChar *nameIdentifier,
+						 const xmlChar *nameQualifier,
+						 const xmlChar *format)
+{
+     LassoNode *identifier;
+
+     identifier = lasso_lib_idp_provided_name_identifier_new();
+     lasso_saml_name_identifier_set_nameQualifier(identifier, nameQualifier);
+     lasso_saml_name_identifier_set_format(identifier, format);
+
+     return(identifier);
+}
+
+LassoNode *lasso_build_spProvidedNameIdentifier(const xmlChar *nameIdentifier,
+						const xmlChar *nameQualifier,
+						const xmlChar *format)
+{
+     LassoNode *identifier;
+
+     identifier = lasso_lib_sp_provided_name_identifier_new();
+     lasso_saml_name_identifier_set_nameQualifier(identifier, nameQualifier);
+     lasso_saml_name_identifier_set_format(identifier, format);
+
+     return(identifier);
+}
+
+LassoNode *lasso_build_oldProvidedNameIdentifier(const xmlChar *nameIdentifier,
+						 const xmlChar *nameQualifier,
+						 const xmlChar *format)
+{
+     LassoNode *identifier;
+
+     identifier = lasso_lib_old_provided_name_identifier_new();
+     lasso_saml_name_identifier_set_nameQualifier(identifier, nameQualifier);
+     lasso_saml_name_identifier_set_format(identifier, format);
+
+     return(identifier);
+}
---
+++ lasso/Attic/protocols/name_identifier.h
@@ -0,0 +1,6 @@
+#ifndef __NAME_IDENTIFIER__
+#define __NAME_IDENTIFIER__
+
+#include <lasso/lasso.h>
+
+#endif /* __NAME_IDENTIFIER__ */
