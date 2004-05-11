1c046dbd78cb7e3b611eeeb28170b593278f9e79
Nicolas Clapies <nclapies@entrouvert.com>
2004-05-11 16:50:24 +0000

    name identifier mapping response low class binding and, examples ...

---
+++ python/xml/py_lib_name_identifier_mapping_response.h
@@ -0,0 +1,42 @@
+/* $Id$ 
+ *
+ * PyLasso -- Python bindings for Lasso library
+ *
+ * Copyright (C) 2004 Entr'ouvert
+ * http://lasso.labs.libre-entreprise.org
+ * 
+ * Author: Valery Febvre <vfebvre@easter-eggs.com>
+ *         Nicolas Clapies <nclapies@entrouvert.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __PYLASSO_PY_LIB_NAME_IDENTIFIER_MAPPING_RESPONSE_H__
+#define __PYLASSO_PY_LIB_NAME_IDENTIFIER_MAPPING_RESPONSE_H__
+
+#include <lasso/xml/lib_name_identifier_mapping_response.h>
+
+typedef struct {
+    PyObject_HEAD
+    LassoLibNameIdentifierMappingResponse *obj;
+} LassoLibNameIdentifierMappingResponse_object;
+
+#define LassoLibNameIdentifierMappingResponse_get(v) (((v) == Py_None) ? NULL : (((LassoLibNameIdentifierMappingResponse_object *)(PyObject_GetAttr(v, PyString_FromString("_o"))))->obj))
+PyObject *LassoLibNameIdentifierMappingResponse_wrap(LassoLibNameIdentifierMappingResponse *response);
+
+PyObject *lib_name_identifier_mapping_response_new(PyObject *self, PyObject *args);
+PyObject *lib_name_identifier_mapping_response_set_consent(PyObject *self, PyObject *args);
+
+#endif /* __PYLASSO_PY_LIB_NAME_IDENTIFIER_MAPPING_RESPONSE_H__ */
