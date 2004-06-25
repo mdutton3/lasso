df4e8f993c29a17e329fff0b3723a91ee3095bd1
Valery Febvre <vfebvre at easter-eggs.com>
2004-06-25 16:42:25 +0000

    Initial commit

---
+++ lasso/Attic/protocols/provider.c
@@ -0,0 +1,143 @@
+/* $Id$
+ *
+ * Lasso - A free implementation of the Liberty Alliance specifications.
+ *
+ * Copyright (C) 2004 Entr'ouvert
+ * http://lasso.entrouvert.org
+ * 
+ * Author: Valery Febvre <vfebvre@easter-eggs.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <lasso/protocols/provider.h>
+
+/*****************************************************************************/
+/* public methods                                                            */
+/*****************************************************************************/
+xmlChar *
+lasso_provider_get_providerID(LassoProvider *provider)
+{
+  return(lasso_node_get_attr_value(provider->metadata, "ProviderID"));
+}
+
+xmlChar *
+lasso_provider_get_singleSignOnProtocolProfile(LassoProvider *provider)
+{
+  return(lasso_node_get_child_content(provider->metadata, "SingleSignOnProtocolProfile", NULL));
+}
+
+xmlChar *
+lasso_provider_get_singleSignOnServiceUrl(LassoProvider *provider)
+{
+  return(lasso_node_get_child_content(provider->metadata, "SingleSignOnServiceUrl", NULL));
+}
+
+xmlChar *lasso_provider_get_singleLogoutProtocolProfile(LassoProvider *provider)
+{
+  return(lasso_node_get_child_content(provider->metadata, "SingleLogoutProtocolProfile", NULL));
+}
+
+xmlChar *lasso_provider_get_singleLogoutServiceUrl(LassoProvider *provider)
+{
+  return(lasso_node_get_child_content(provider->metadata, "SingleLogoutServiceUrl", NULL));
+}
+
+xmlChar *lasso_provider_get_singleLogoutServiceReturnUrl(LassoProvider *provider)
+{
+  return(lasso_node_get_child_content(provider->metadata, "SingleLogoutServiceReturnUrl", NULL));
+}
+
+/*****************************************************************************/
+/* private methods                                                           */
+/*****************************************************************************/
+
+static xmlChar *lasso_provider_get_direct_child_content(LassoProvider *provider, 
+							const xmlChar *name)
+{
+  LassoNode *node;
+  
+  node = lasso_node_get_child(LASSO_NODE(provider), name, NULL);
+  if(!node)
+    return(NULL);
+  return(lasso_node_get_content(node));
+}
+
+
+/*****************************************************************************/
+/* instance and class init functions                                         */
+/*****************************************************************************/
+
+static void
+lasso_provider_instance_init(LassoProvider *provider)
+{
+    LassoNodeClass *class = LASSO_NODE_GET_CLASS(LASSO_NODE(provider));
+
+    class->set_name(LASSO_NODE(provider), "Provider");
+}
+
+static void
+lasso_provider_class_init(LassoProviderClass *klass) {
+}
+
+GType lasso_provider_get_type() {
+  static GType this_type = 0;
+
+  if (!this_type) {
+    static const GTypeInfo this_info = {
+      sizeof (LassoProviderClass),
+      NULL,
+      NULL,
+      (GClassInitFunc) lasso_provider_class_init,
+      NULL,
+      NULL,
+      sizeof(LassoProvider),
+      0,
+      (GInstanceInitFunc) lasso_provider_instance_init,
+    };
+    
+    this_type = g_type_register_static(LASSO_TYPE_NODE,
+				       "LassoProvider",
+				       &this_info, 0);
+  }
+  return this_type;
+}
+
+LassoNode* lasso_provider_new(){
+     LassoNode *provider;
+
+     provider = LASSO_NODE(g_object_new(LASSO_TYPE_PROVIDER, NULL));
+
+     return (provider);
+}
+
+LassoProvider *lasso_provider_new_from_filename(char *filename){
+     LassoProvider *provider;
+     xmlDocPtr  doc;
+     xmlNodePtr root;
+
+     provider = g_object_new(LASSO_TYPE_PROVIDER, NULL);
+
+     /* get root element of doc and duplicate it */
+     doc = xmlParseFile(filename);
+     root = xmlCopyNode(xmlDocGetRootElement(doc), 1);
+     xmlFreeDoc(doc);
+     provider->metadata = lasso_node_new_from_xmlNode(root);
+
+     provider->public_key = NULL;
+     provider->certificate = NULL;
+
+     return(provider);
+}
---
+++ lasso/id-ff/server.c
@@ -0,0 +1,124 @@
+/* $Id$
+ *
+ * Lasso - A free implementation of the Liberty Alliance specifications.
+ *
+ * Copyright (C) 2004 Entr'ouvert
+ * http://lasso.entrouvert.org
+ * 
+ * Author: Valery Febvre <vfebvre@easter-eggs.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <lasso/environs/server.h>
+
+/*****************************************************************************/
+/* public methods                                                            */
+/*****************************************************************************/
+
+gint
+lasso_server_add_provider_from_file(LassoServer *server,
+				    gchar       *filename)
+{
+  LassoProvider *provider;
+  
+  provider = lasso_provider_new_from_filename(filename);
+  g_ptr_array_add(server->providers, provider);
+  
+  return (1);
+}
+
+LassoProvider*
+lasso_server_get_provider(LassoServer *server,
+			  gchar       *providerID)
+{
+  LassoProvider *provider;
+  char *id;
+  int index, len;
+  
+  len = server->providers->len;
+  for(index = 0; index<len; index++) {
+    provider = g_ptr_array_index(server->providers, index);
+    
+    id = lasso_provider_get_providerID(provider);
+    if (!strcmp(providerID, id)) {
+      return(provider);
+    }
+  }
+  
+  return(NULL);
+}
+
+gint
+lasso_server_set_security(gchar *private_key,
+			  gchar *public_key,
+			  gchar *certificate)
+{
+  g_return_if_fail(private_key);
+  g_return_if_fail(public_key);
+  g_return_if_fail(certificate);
+
+}
+
+/*****************************************************************************/
+/* instance and class init functions                                         */
+/*****************************************************************************/
+
+static void
+lasso_server_instance_init(LassoServer *server)
+{
+  server->providers = g_ptr_array_new();
+
+  server->private_key = NULL;
+  server->public_key = NULL;
+  server->certificate = NULL;
+}
+
+static void
+lasso_server_class_init(LassoServerClass *klass){
+}
+
+GType lasso_server_get_type() {
+  static GType this_type = 0;
+
+  if (!this_type) {
+    static const GTypeInfo this_info = {
+      sizeof (LassoServerClass),
+      NULL,
+      NULL,
+      (GClassInitFunc) lasso_server_class_init,
+      NULL,
+      NULL,
+      sizeof(LassoServer),
+      0,
+      (GInstanceInitFunc) lasso_server_instance_init,
+    };
+    
+    this_type = g_type_register_static(G_TYPE_OBJECT,
+				       "LassoServer",
+				       &this_info, 0);
+  }
+  return this_type;
+}
+
+LassoServer *lasso_server_new()
+{
+  LassoServer *server;
+
+  server = g_object_new(LASSO_TYPE_SERVER, NULL);
+
+  return(server);
+
+}
