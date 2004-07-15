4c6de60cca9603de5fd58655e0e8bc1a01c830ae
Valery Febvre <vfebvre at easter-eggs.com>
2004-07-15 10:42:05 +0000

    4 # define moved from .h to .c

--- lasso/Attic/protocols/identity.c
+++ lasso/Attic/protocols/identity.c
@@ -24,6 +24,11 @@
 
 #include <lasso/protocols/identity.h>
 
+#define LASSO_IDENTITY_NODE "LassoIdentity"
+#define LASSO_IDENTITY_REMOTE_PROVIDERID_NODE "RemoteProviderID"
+#define LASSO_IDENTITY_LOCAL_NAME_IDENTIFIER_NODE "LassoLocalNameIdentifier"
+#define LASSO_IDENTITY_REMOTE_NAME_IDENTIFIER_NODE "LassoRemoteNameIdentifier"
+
 /*****************************************************************************/
 /* public methods                                                            */
 /*****************************************************************************/
