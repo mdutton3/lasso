995db11b18b151ff4a07b44708fdcd130d8ac161
Valery Febvre <vfebvre at easter-eggs.com>
2004-07-16 14:04:32 +0000

    Removed 2 lasso_node_copy() in lasso_identity_set_local_nameIdentifier() and lasso_identity_set_remote_nameIdentifier()

--- lasso/Attic/protocols/identity.c
+++ lasso/Attic/protocols/identity.c
@@ -78,14 +78,14 @@ void
 lasso_identity_set_local_nameIdentifier(LassoIdentity *identity,
 					LassoNode     *nameIdentifier)
 {
-  identity->local_nameIdentifier = lasso_node_copy(nameIdentifier);
+  identity->local_nameIdentifier = nameIdentifier;
 }
 
 void
 lasso_identity_set_remote_nameIdentifier(LassoIdentity *identity,
 					 LassoNode     *nameIdentifier)
 {
-  identity->remote_nameIdentifier = lasso_node_copy(nameIdentifier);
+  identity->remote_nameIdentifier = nameIdentifier;
 }
 
 gboolean
