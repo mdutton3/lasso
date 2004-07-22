27577a7ccfc6dcf8de244e9b2b80c2ac8924ec16
Frederic Peters <fpeters@entrouvert.com>
2004-07-22 10:11:51 +0000

    missing includes

--- lasso/Attic/protocols/authn_response.c
+++ lasso/Attic/protocols/authn_response.c
@@ -23,6 +23,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <string.h>
+#include <xmlsec/base64.h>
 #include <lasso/protocols/authn_response.h>
 
 static void
