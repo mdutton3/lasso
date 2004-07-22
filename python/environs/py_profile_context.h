81b5acd5eb4528e10d3da2045ce8b0887effeabc
Nicolas Clapies <nclapies@entrouvert.com>
2004-07-22 09:54:05 +0000

    profile context with function to parse a soap request

---
+++ python/environs/py_profile_context.h
@@ -0,0 +1,35 @@
+/* $Id$ 
+ *
+ * PyLasso -- Python bindings for Lasso library
+ *
+ * Copyright (C) 2004 Entr'ouvert
+ * http://lasso.labs.libre-entreprise.org
+ * 
+ * Authors: Valery Febvre <vfebvre@easter-eggs.com>
+ *          Nicolas Clapies <nclapies@entrouvert.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __PYLASSO_PY_PROFILE_CONTEXT_H__
+#define __PYLASSO_PY_PROFILE_CONTEXT_H__
+
+#include <lasso/environs/profile_context.h>
+
+PyObject *profile_context_get_request_type_from_soap_msg(PyObject *self, PyObject *args);
+
+#endif /* __PYLASSO_PY_PROFILE_CONTEXT_H__ */
+
+
