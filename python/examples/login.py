831277c6b7da16ea21508c27d35dfe4d020c9167
Nicolas Clapies <nclapies@entrouvert.com>
2004-07-26 23:50:03 +0000

    remove the second param of lasso.Login.new

--- python/examples/login.py
+++ python/examples/login.py
@@ -22,7 +22,7 @@ server.destroy()
 
 # create AuthnRequest
 server = lasso.Server.new_from_dump(server_dump)
-splogin = lasso.Login.new(server, None)
+splogin = lasso.Login.new(server)
 ret = splogin.init_authn_request("https://identity-provider:2003/liberty-alliance/metadata")
 splogin.request.set_isPassive(0)
 splogin.request.set_forceAuthn(1)
@@ -44,7 +44,7 @@ server.add_provider("../../examples/sp.xml",
                     "../../examples/rsapub.pem", "../../examples/rsacert.pem")
 
 # create AuthnResponse OR artifact (depending ProtocolProfile)
-idplogin = lasso.Login.new(server, None)
+idplogin = lasso.Login.new(server)
 
 # get query part in msg_url
 authn_request_msg = string.split(splogin.msg_url, '?')[1]
@@ -73,7 +73,7 @@ server = lasso.Server.new("../../examples/sp.xml",
 server.add_provider("../../examples/idp.xml", None, None)
 
 # create Request OR finish (if an authnResponse was received)
-splogin = lasso.Login.new(server, None)
+splogin = lasso.Login.new(server)
 
 response_msg = string.split(idplogin.msg_url, '?')[1]
 ret = splogin.init_request(response_msg,
@@ -93,7 +93,7 @@ server.add_provider("../../examples/sp.xml",
                     "../../examples/rsapub.pem", "../../examples/rsacert.pem")
 
 # create Response
-idplogin = lasso.Login.new(server, None)
+idplogin = lasso.Login.new(server)
 
 ret = idplogin.process_request_msg(splogin.msg_body)
 print "samlp:AssertionArtifact = %s" % idplogin.assertionArtifact
