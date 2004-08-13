8836ee6979c26484bcd31043b2f3ce92ddfb3b8b
Frederic Peters <fpeters@entrouvert.com>
2004-08-13 18:16:03 +0000

    include xmlsec/base64.h since it uses one of those functions

--- lasso/id-ff/server.c
+++ lasso/id-ff/server.c
@@ -23,6 +23,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <xmlsec/base64.h>
+
 #include <lasso/xml/errors.h>
 #include <lasso/environs/server.h>
 
