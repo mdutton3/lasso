54b4251de940ae0b876c4b60e7ee4e793e33ad90
Frederic Peters <fpeters@entrouvert.com>
2004-08-17 17:09:27 +0000

    this script checks a documentation file for functions that do not exist

---
+++ docs/lasso-book/check-functions.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+#
+# Checks a documentation file for functions that do not exist
+
+import re
+import sys
+
+functions = {}
+for line in file(sys.argv[1]):
+    if not "lasso_" in line:
+        continue
+    if not "(" in line:
+        continue
+    for f in re.findall(r"(lasso_[a-zA-Z_]+?)\(", line):
+        functions[f] = 1
+
+known_symbols = [x.strip() for x in file("../reference/lasso-decl-list.txt")]
+
+for f in functions:
+    if not f in known_symbols:
+        print f
+
