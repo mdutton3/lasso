b767d59e796544a0aff87eb6bd7543586e4433e0
Frederic Peters <fpeters@entrouvert.com>
2004-08-25 08:33:59 +0000

    added Defederation profile; workaround lasso-sharp.snk

--- csharp/AssemblyInfo.cs
+++ csharp/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly:AssemblyDelaySign(false)]
-[assembly:AssemblyKeyFile("lasso-sharp.snk")]
+[assembly:AssemblyKeyFile("lasso-sharp-build.snk")]
 
