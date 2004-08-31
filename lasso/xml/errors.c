de018d15738a699cabc7d482b405f90109b219ed
Nicolas Clapies <nclapies@entrouvert.com>
2004-08-31 12:05:04 +0000

    Added code error : LASSO_PROFILE_ERROR_MISSING_REQUEST. It indicates that lasso needs a request to process a task. For example, when building a response message

--- lasso/xml/errors.c
+++ lasso/xml/errors.c
@@ -71,6 +71,8 @@ lasso_strerror(int error_code)
 
   case LASSO_PROFILE_ERROR_INVALID_QUERY:
     return "Error while parsing query message\n";
+  case LASSO_PROFILE_ERROR_MISSING_REQUEST:
+    return "Missing request\n";
 
   case LASSO_PARAM_ERROR_BAD_TYPE_OR_NULL_OBJ:
     return "An object type provided as parameter is invalid or object is NULL.\n";
