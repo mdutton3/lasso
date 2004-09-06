7f96eb8e68c38f03cc927dc688419892e801ee96
Nicolas Clapies <nclapies@entrouvert.com>
2004-09-06 15:30:54 +0000

    *** empty log message ***

--- lasso/id-ff/logout.h
+++ lasso/id-ff/logout.h
@@ -70,7 +70,7 @@ LASSO_EXPORT GType        lasso_logout_get_type               (void);
 LASSO_EXPORT LassoLogout* lasso_logout_new                    (LassoServer       *server,
 							       lassoProviderType  provider_type);
 
-LassoLogout*              lasso_logout_new_from_dump          (LassoServer *server, gchar *dump);
+LASSO_EXPORT LassoLogout* lasso_logout_new_from_dump          (LassoServer *server, gchar *dump);
 
 LASSO_EXPORT gint         lasso_logout_build_request_msg      (LassoLogout *logout);
 
