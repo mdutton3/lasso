f1709c77a10f83de649eec95574910e9075804a0
Nicolas Clapies <nclapies@entrouvert.com>
2004-12-10 16:58:11 +0000

    Initial version : support of personal profile msg contact. It is very experimental and only supports msg account and msg provider part.

---
+++ lasso/xml/pp_msg_contact.h
@@ -0,0 +1,69 @@
+/* $Id$ 
+ *
+ * Lasso - A free implementation of the Liberty Alliance specifications.
+ *
+ * Copyright (C) 2004 Entr'ouvert
+ * http://lasso.entrouvert.org
+ * 
+ * Authors: Nicolas Clapies <nclapies@entrouvert.com>
+ *          Valery Febvre <vfebvre@easter-eggs.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __LASSO_PP_MSG_CONTACT_H__
+#define __LASSO_PP_MSG_CONTACT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */ 
+
+#include <lasso/xml/xml.h>
+
+#define LASSO_TYPE_PP_MSG_CONTACT (lasso_pp_msg_contact_get_type())
+#define LASSO_PP_MSG_CONTACT(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), \
+			LASSO_TYPE_PP_MSG_CONTACT, LassoPPMsgContact))
+#define LASSO_PP_MSG_CONTACT_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), \
+			LASSO_TYPE_PP_MSG_CONTACT, LassoPPMsgContactClass))
+#define LASSO_IS_PP_MSG_CONTACT(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), LASSO_TYPE_PP_MSG_CONTACT))
+#define LASSO_IS_PP_MSG_CONTACT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), \
+       LASSO_TYPE_PP_MSG_CONTACT))
+#define LASSO_PP_MSG_CONTACT_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), \
+       LASSO_TYPE_PP_MSG_CONTACT, LassoPPMsgContactClass)) 
+
+typedef struct _LassoPPMsgContact LassoPPMsgContact;
+typedef struct _LassoPPMsgContactClass LassoPPMsgContactClass;
+
+struct _LassoPPMsgContact {
+	LassoNode parent;
+
+	/* Is email MsgAccount@MsgProvider ? */
+	char *MsgProvider;
+	char *MsgAccount;
+};
+
+struct _LassoPPMsgContactClass {
+	LassoNodeClass parent;
+};
+
+LASSO_EXPORT GType lasso_pp_msg_contact_get_type(void);
+
+LASSO_EXPORT LassoPPMsgContact* lasso_pp_msg_contact_new(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __LASSO_PP_MSG_CONTACT_H__ */
