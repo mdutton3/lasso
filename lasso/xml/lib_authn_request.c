548c81feb3e794c947b70b8a0b3cba75705a7fe9
Frederic Peters <fpeters@entrouvert.com>
2005-01-21 21:27:47 +0000

    removed lib:Scoping, optional

--- lasso/xml/lib_authn_request.c
+++ lasso/xml/lib_authn_request.c
@@ -168,7 +168,7 @@ instance_init(LassoLibAuthnRequest *node)
 	node->AssertionConsumerServiceID = NULL;
 	node->RequestAuthnContext = NULL;
 	node->RelayState = NULL;
-	node->Scoping = lasso_lib_scoping_new();
+	node->Scoping = NULL;
 	node->consent = NULL;
 }
 
