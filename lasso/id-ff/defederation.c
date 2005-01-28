22b3a159a1aa09d152894d79d03e0bfd22a584dc
Frederic Peters <fpeters@entrouvert.com>
2005-01-28 13:18:29 +0000

    include missing identityprivate.h

--- lasso/id-ff/defederation.c
+++ lasso/id-ff/defederation.c
@@ -26,6 +26,7 @@
 
 #include <lasso/id-ff/providerprivate.h>
 #include <lasso/id-ff/sessionprivate.h>
+#include <lasso/id-ff/identityprivate.h>
 
 /*****************************************************************************/
 /* public methods                                                            */
