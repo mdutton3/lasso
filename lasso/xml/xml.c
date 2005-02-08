f83d664cbf1d1d7d772f1ac13f50c0967f91f57d
Frederic Peters <fpeters@entrouvert.com>
2005-02-08 12:30:54 +0000

    that inline should be ok everywhere

--- lasso/xml/xml.c
+++ lasso/xml/xml.c
@@ -1406,11 +1406,7 @@ xmlDeclareNs(xmlNode *root_node, xmlNode *node)
 			xmlDeclareNs(root_node, t);
 }
 
-#if defined(__GNUC__)
-__inline__ static int
-#else
-__inline static int
-#endif
+static inline int
 sameNs(xmlNs *ns1, xmlNs *ns2)
 {
 	/* this checks ns->prefix instead of ns->href so it is possible to
