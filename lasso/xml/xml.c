da39ad6e5882e2912a5292fd2dcd0549e7509826
Frederic Peters <fpeters@entrouvert.com>
2005-05-10 18:17:32 +0000

    \r may be hiding in base64

--- lasso/xml/xml.c
+++ lasso/xml/xml.c
@@ -794,8 +794,8 @@ is_base64(const char *message)
 	const char *c;
 
 	c = message;
-	while (*c != 0 && (isalnum(*c) || *c == '+' || *c == '/' || *c == '\n')) c++;
-	while (*c == '=' || *c == '\n') c++; /* trailing = */
+	while (*c != 0 && (isalnum(*c) || *c == '+' || *c == '/' || *c == '\n' || *c == '\r')) c++;
+	while (*c == '=' || *c == '\n' | *c == '\r') c++; /* trailing = */
 
 	if (*c == 0)
 		return TRUE;
