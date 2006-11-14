fc8a4b153136ac1bc3d555bfa19e09cb8709c283
Frederic Peters <fpeters@entrouvert.com>
2006-11-14 14:37:58 +0000

    include header from xmlsec to get xmlSecCreateTree declaration

--- lasso/id-wsf/discovery.c
+++ lasso/id-wsf/discovery.c
@@ -22,6 +22,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <xmlsec/xmltree.h>
+
 #include <lasso/xml/soap_binding_correlation.h>
 #include <lasso/xml/saml_assertion.h>
 #include <lasso/xml/saml_attribute_value.h>
