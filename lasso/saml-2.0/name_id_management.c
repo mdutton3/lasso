05212816bda065c4f14431967f802855a765f4ce
Frederic Peters <fpeters@entrouvert.com>
2007-01-08 13:36:00 +0000

    fixed docstring parameter name

--- lasso/saml-2.0/name_id_management.c
+++ lasso/saml-2.0/name_id_management.c
@@ -37,7 +37,7 @@
 /**
  * lasso_name_id_management_init_request:
  * @name_id_management: a #LassoNameIdManagement
- * @remote_providerID: the providerID of the remote provider.
+ * @remote_provider_id: the providerID of the remote provider.
  * @new_name_id: the new NameId or NULL to terminate a federation
  * @http_method: if set, then it get the protocol profile in metadata
  *     corresponding of this HTTP request method.
