bb5b171a66f07d68fb8746fbb501059e3a6cc959
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2008-02-20 14:01:57 +0000

    * Add lasso_ds_* API to the general build of lasso (was enable only with id-wsf before)

--- lasso/extract_symbols.py
+++ lasso/extract_symbols.py
@@ -26,7 +26,7 @@ for header_file in glob.glob('%s/*/*.h' % srcdir) + glob.glob('%s/*.h' % srcdir)
 wsf = ['lasso_disco_', 'lasso_dst_', 'lasso_is_', 'lasso_profile_service_',
         'lasso_discovery', 'lasso_wsf', 'lasso_interaction_', 'lasso_utility_',
         'lasso_sa_', 'lasso_soap_', 'lasso_authentication', 'lasso_wsse_',
-        'lasso_sec_', 'lasso_ds_', 'lasso_idwsf2', 'lasso_wsf2', 'lasso_wsa_',
+        'lasso_sec_', 'lasso_idwsf2', 'lasso_wsf2', 'lasso_wsa_',
         'lasso_wsu_']
 if enable_wsf:
     wsf = []
