3ab9da11203b5b4e0d79a7c3a22408ac62545aa1
Damien Laniel <dlaniel@entrouvert.com>
2008-05-21 23:29:14 +0000

    fixed freeing svcMDID list

--- lasso/id-ff/identity.c
+++ lasso/id-ff/identity.c
@@ -408,7 +408,7 @@ dispose(GObject *object)
 	identity->private_data->dispose_has_run = TRUE;
 
 	if (identity->private_data->svcMDID != NULL) {
-		/* XXX: must fix content */
+		g_list_foreach(identity->private_data->svcMDID, (GFunc)g_free, NULL);
 		g_list_free(identity->private_data->svcMDID);
 		identity->private_data->svcMDID = NULL;
 	}
