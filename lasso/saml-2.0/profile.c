cf4cf42f1aad394a20893c4bedfba863ef846575
Damien Laniel <dlaniel@entrouvert.com>
2008-05-21 22:13:16 +0000

    added a missing reference

--- lasso/saml-2.0/profile.c
+++ lasso/saml-2.0/profile.c
@@ -336,8 +336,8 @@ lasso_saml20_profile_set_session_from_dump_decrypt(
 	}
 
 	if (assertion->Subject != NULL && assertion->Subject->EncryptedID != NULL) {
-		assertion->Subject->NameID = LASSO_SAML2_NAME_ID(
-				assertion->Subject->EncryptedID->original_data);
+		assertion->Subject->NameID = g_object_ref(
+			assertion->Subject->EncryptedID->original_data);
 		g_object_unref(assertion->Subject->EncryptedID);
 		assertion->Subject->EncryptedID = NULL;
 	}
