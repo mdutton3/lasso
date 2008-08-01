bfe206c67f8a6fa50cb75320045ef0c580dbf834
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2008-08-01 14:06:45 +0000

    add soap binding util function to lookup specific SOAP header for ID-WSF SOAP encapsulation

---
+++ lasso/soap_binding.h
@@ -0,0 +1,43 @@
+/* $Id$ 
+ *
+ * Lasso - A free implementation of the Liberty Alliance specifications.
+ *
+ * Copyright (C) 2004-2007 Entr'ouvert
+ * http://lasso.entrouvert.org
+ * 
+ * Authors: See AUTHORS file in top-level directory.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __LASSO_SOAP_BINDING_H__
+#define __LASSO_SOAP_BINDING_H__
+
+#include <lasso/xml/soap_binding_provider.h>
+#include <lasso/xml/soap_binding_correlation.h>
+#include <lasso/xml/soap_envelope.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */ 
+
+LASSO_EXPORT LassoSoapBindingProvider* lasso_soap_binding_get_provider(LassoSoapEnvelope *envelope);
+
+LASSO_EXPORT LassoSoapBindingCorrelation* lasso_soap_binding_get_correlation(LassoSoapEnvelope *evelope);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */ 
+#endif /* __LASSO_SOAP_BINDING_H__ */
