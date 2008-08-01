5b6c85419bd31bfb0b191b28cd66f84d2bbd83ae
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2008-08-01 14:10:19 +0000

    add an new error for a missing ResourceID inside a ResourceOffering

--- lasso/errors.h
+++ lasso/errors.h
@@ -150,6 +150,7 @@
 #define LASSO_WSF_PROFILE_ERROR_SOAP_FAULT            1104 /* SOAP ID-WSF binding returned a SOAP fault */
 #define LASSO_WSF_PROFILE_ERROR_UNSUPPORTED_SECURITY_MECHANISM 1105 /* The specified security mechanism is not supported by lasso ID-WSF library */
 #define LASSO_WSF_PROFILE_ERROR_MISSING_DESCRIPTION     -1106 /* No ID-WSF web Service description could be found for the current security mechanism */
+#define LASSO_WSF_PROFILE_ERROR_MISSING_RESOURCE_ID  -1107 /* The necessary ResourceID or EncryptedResourceID for calling an ID-WSF service is missing. */
 
 /* ID-WSF 2 Discovery */
 #define LASSO_DISCOVERY_ERROR_SVC_METADATA_REGISTER_FAILED        -1200 /* Service metadata registration failed */
