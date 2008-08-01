c89267b8feb497d7525b53d066054d4911cd466a
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2008-08-01 14:11:53 +0000

    * specialise lasso_node_new_from_xmlNode for soap:detail node

--- lasso/xml/xml.c
+++ lasso/xml/xml.c
@@ -1630,6 +1630,8 @@ lasso_node_new_from_xmlNode(xmlNode *xmlnode)
 		typename = g_strdup("LassoIdWsf2UtilStatus");
 	} else if (strcmp(prefix, "WsSec1") == 0 && strcmp(node_name, "Security") == 0) {
 		typename = g_strdup("LassoWsSec1SecurityHeader");
+	} else if (strcmp(prefix, "Soap") == 0 && strcmp(node_name, "detail") == 0) {
+		typename = g_strdup("LassoSoapDetail");
 	} else {
 		typename = g_strdup_printf("Lasso%s%s", prefix, node_name);
 	}
