530e6ceac03ec2a6487c00f34faf4a66a16a3736
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2009-04-27 08:19:08 +0000

    Complete documentation of lasso_login_build_authn_request
     * lasso/id-ff/login.c:
       add all possible return codes with meaning.

--- lasso/id-ff/login.c
+++ lasso/id-ff/login.c
@@ -938,14 +938,26 @@ lasso_login_build_artifact_msg(LassoLogin *login, LassoHttpMethod http_method)
  * lasso_login_build_authn_request_msg:
  * @login: a #LassoLogin
  *
- * Converts profile authentication request (@request member) into a Liberty
- * message, either an URL in HTTP-Redirect profile or an URL and a field value
- * in Browser-POST (form) profile.
+ * Converts profile authentication request (@request member) into a Liberty message, either an URL
+ * in HTTP-Redirect profile or an URL and a field value in Browser-POST (form) profile.
  *
- * The URL is set into the @msg_url member and the eventual field value (LAREQ)
- * is set into the @msg_body member.
+ * The URL is set into the @msg_url member and the eventual field value (LAREQ) is set into the
+ * @msg_body member.
  *
- * Return value: 0 on success; or a negative value otherwise.
+ * Return value: 0 on success; or
+ * LASSO_PARAM_ERROR_BAD_TYPE_OR_NULL_OBJ if login is not a #LassoLogin object,
+ * LASSO_PROFILE_ERROR_MISSING_REMOTE_PROVIDERID if not remote provider ID was setup -- it usually
+ * means that lasso_login_init_request was not called before,
+ * LASSO_SERVER_ERROR_PROVIDER_NOT_FOUND if the remote provider ID is not registered in the server
+ * object,
+ * LASSO_PROFILE_ERROR_UNSUPPORTED_PROFILE if the SSO profile is not supported by the targeted
+ * provider,
+ * LASSO_PROFILE_ERROR_BUILDING_QUERY_FAILED if the building of the query part of the
+ * redirect URL or of the body of the POST content failed -- it only happens with
+ * LASSO_HTTP_METHOD_REDIRECT of LASSO_HTTP_METHOD_POST --,
+ * LASSO_PROFILE_ERROR_UNKNOWN_PROFILE_URL if the metadata of the remote provider does not contain
+ * an url for the SSO profile.
+ *
  **/
 gint
 lasso_login_build_authn_request_msg(LassoLogin *login)
