7071a9ce9a0ae6f81fd5ba53129064a8b7fb4a4d
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2009-04-30 14:58:17 +0000

    Export the new flag
     * lasso/debug.h:
       export new flag lasso_flag_sign_messages.

--- lasso/debug.h
+++ lasso/debug.h
@@ -36,6 +36,7 @@ LASSO_EXPORT extern gboolean lasso_flag_verify_signature;
 LASSO_EXPORT extern gboolean lasso_flag_memory_debug;
 LASSO_EXPORT extern gboolean lasso_flag_strict_checking;
 LASSO_EXPORT extern gboolean lasso_flag_add_signature;
+LASSO_EXPORT extern gboolean lasso_flag_sign_messages;
 
 #ifdef __cplusplus
 }
