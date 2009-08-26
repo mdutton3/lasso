16a136c61b84f14e84e72fb1d0f0c09dc0c1beec
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2009-08-26 15:14:59 +0000

    Tests: do not hardcode PYTHONPATH in the valgrind wrapper
     * tests/integration/valgrind-wrapper.sh: remove PYTHONPATH setting.

--- tests/integration/valgrind-wrapper.sh
+++ tests/integration/valgrind-wrapper.sh
@@ -5,6 +5,6 @@ if [ $1 == 'python' ]; then
 else
 	NAME=$1
 fi
-env MALLOC_CHECK_=2 G_SLICE=always-malloc PYTHONPATH=/home/bdauvergne/wd/lasso/git/bindings/python:/home/bdauvergne/wd/lasso/git/bindings/python/.libs LD_LIBRARY_PATH=/home/bdauvergne/wd/lasso/git/lasso/.libs valgrind --show-reachable=yes --suppressions=../valgrind/lasso.supp --suppressions=../valgrind/glib.supp --suppressions=../valgrind/openssl.supp --suppressions=/usr/lib/valgrind/python.supp --leak-check=full --log-file="${NAME}_${DATE}_pid-$$.log" --track-origins=yes "$@"
+env MALLOC_CHECK_=2 G_SLICE=always-malloc valgrind --show-reachable=yes --suppressions=../valgrind/lasso.supp --suppressions=../valgrind/glib.supp --suppressions=../valgrind/openssl.supp --suppressions=/usr/lib/valgrind/python.supp --leak-check=full --log-file="${NAME}_${DATE}_pid-$$.log" --track-origins=yes "$@"
 
 
