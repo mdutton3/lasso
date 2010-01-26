fc69229a4ccd22e1d65858342e395214c047fb2e
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-01-26 10:39:24 +0000

    in lasso/saml-2.0/logout.c, fix missing declaration

--- lasso/saml-2.0/logout.c
+++ lasso/saml-2.0/logout.c
@@ -326,7 +326,7 @@ lasso_saml20_logout_build_response_msg(LassoLogout *logout)
 
 	/* SP initiated logout */
 	if (logout->initial_remote_providerID) {
-		remote_provider = lasso_server_get_provider(profile->server, profile->remote_providerID);
+		LassoProvider* remote_provider = lasso_server_get_provider(profile->server, profile->remote_providerID);
 		if (remote_provider->role & LASSO_PROVIDER_ROLE_SP) {
 			lasso_transfer_string(profile->remote_providerID,
 					logout->initial_remote_providerID);
