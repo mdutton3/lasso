2f15b4431d442cb815d681c37c4a2c9a6e3cec3a
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-02-12 09:48:22 +0000

    Add LASSO_SAML2_FIELD_ENCODING
     * lasso/xml/saml-2.0/saml2_strings.h:
       add another field name from SAML 2.0 specifications.

--- lasso/xml/saml-2.0/saml2_strings.h
+++ lasso/xml/saml-2.0/saml2_strings.h
@@ -393,6 +393,7 @@
 #define LASSO_SAML2_CONFIRMATION_METHOD_HOLDER_OF_KEY "urn:oasis:names:tc:SAML:2.0:cm:holder-of-key"
 
 /* POST and GET request fields */
+#define LASSO_SAML2_FIELD_ENCODING "SAMLEncoding"
 #define LASSO_SAML2_FIELD_RESPONSE "SAMLResponse"
 #define LASSO_SAML2_FIELD_REQUEST "SAMLRequest"
 #define LASSO_SAML2_FIELD_ARTIFACT "SAMLart"
