b785881e531116da7250190e632bd205212a9bdf
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-11-22 18:50:35 +0100

    [integration tests] does not use full leak report

--- tests/integration/valgrind-wrapper.sh
+++ tests/integration/valgrind-wrapper.sh
@@ -5,6 +5,6 @@ if [ $1 == 'python' ]; then
 else
 	NAME=$1
 fi
-env G_DEBUG=gc-friendly MALLOC_CHECK_=2 G_SLICE=always-malloc valgrind --show-reachable=yes --suppressions=../valgrind/lasso.supp --suppressions=../valgrind/glib.supp --suppressions=../valgrind/openssl.supp --suppressions=/usr/lib/valgrind/python.supp --leak-check=full --log-file="${NAME}_${DATE}_pid-$$.log" --track-origins=yes --num-callers=50 "$@"
+env G_DEBUG=gc-friendly MALLOC_CHECK_=2 G_SLICE=always-malloc valgrind --show-reachable=yes --suppressions=../valgrind/lasso.supp --suppressions=../valgrind/glib.supp --suppressions=../valgrind/openssl.supp --suppressions=/usr/lib/valgrind/python.supp --log-file="${NAME}_${DATE}_pid-$$.log" --track-origins=yes --num-callers=50 "$@"
 
 
