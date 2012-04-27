d3c702e130ab654fd47081c1460f2f8527d10bc8
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-04-27 15:54:51 +0200

    fix wrong EXTRA_DIST in lasso/xml/dsig/Makefile.am

---
+++ lasso/xml/dsig/xml_dsig.h
@@ -0,0 +1,34 @@
+/* $Id$
+ *
+ * Lasso - A free implementation of the Liberty Alliance specifications.
+ *
+ * Copyright (C) 2004-2007 Entr'ouvert
+ * http://lasso.entrouvert.org
+ *
+ * Authors: See AUTHORS file in top-level directory.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __LASSO_XML_DSIG_H__
+#define __LASSO_XML_DSIG_H__
+
+#include "ds_key_info.h"
+#include "ds_key_value.h"
+#include "ds_rsa_key_value.h"
+#include "ds_x509_data.h"
+#include "strings.h"
+
+#endif /* __LASSO_XML_DSIG_H__ */
