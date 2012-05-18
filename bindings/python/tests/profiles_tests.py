1d1bba7ba1cb747d15a5b8aafc5480f44e5a5cb0
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-05-18 14:02:42 +0200

    [tests][python/tests] add tests data and script

--- bindings/python/tests/profiles_tests.py
+++ bindings/python/tests/profiles_tests.py
@@ -43,6 +43,17 @@ except NameError:
     srcdir = os.environ.get('srcdir', '.')
     dataDir = '%s/../../../tests/data' % srcdir
 
+def server(local_name, remote_role, remote_name):
+    pwd = os.path.join(dataDir, local_name, 'password')
+    password = None
+    if os.path.exists(pwd):
+        password = file(pwd).read()
+    s = lasso.Server(os.path.join(dataDir, local_name, 'metadata.xml'),
+            os.path.join(dataDir, local_name, 'private-key.pem'),
+            password)
+    s.addProvider(remote_role, os.path.join(dataDir, remote_name, 'metadata.xml'))
+    return s
+
 
 class ServerTestCase(unittest.TestCase):
     def test01(self):
@@ -208,6 +219,24 @@ class LoginTestCase(unittest.TestCase):
         self.failUnless('<action2>do action 2</action2>' in extensionsList[0])
         self.failUnless('<action3>do action 3</action3>' in extensionsList[0])
 
+    def test_05(self):
+        '''Login test between SP and IdP with encrypted private keys'''
+        sp_server = server('sp7-saml2', lasso.PROVIDER_ROLE_IDP, 'idp7-saml2')
+        idp_server = server('idp7-saml2', lasso.PROVIDER_ROLE_SP, 'sp7-saml2')
+
+        sp_login = lasso.Login(sp_server)
+        sp_login.initAuthnRequest()
+        sp_login.request.protocolBinding = lasso.SAML2_METADATA_BINDING_POST;
+        sp_login.buildAuthnRequestMsg()
+        idp_login = lasso.Login(idp_server)
+        # idp_login.setSignatureVerifyHint(lasso.PROFILE_SIGNATURE_VERIFY_HINT_FORCE)
+        idp_login.processAuthnRequestMsg(sp_login.msgUrl.split('?')[1])
+        idp_login.validateRequestMsg(True, True)
+        idp_login.buildAssertion("None", "None", "None", "None", "None")
+        idp_login.buildAuthnResponseMsg()
+        # sp_login.setSignatureVerifyHint(lasso.PROFILE_SIGNATURE_VERIFY_HINT_FORCE)
+        sp_login.processAuthnResponseMsg(idp_login.msgBody)
+        sp_login.acceptSso()
 
 class LogoutTestCase(unittest.TestCase):
     def test01(self):
