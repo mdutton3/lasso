mdutton3/lasso

commit 4789e8d4d68eb4134292e7fa6d6425db91b501f8

Trim unused code
Clang complains these values are never used, avoid even assigning them.

License: MIT
Signed-off-by: Simo Sorce <simo@redhat.com>

lasso/saml-2.0/profile.c
@@ -1165,7 +1165,7 @@ lasso_saml20_profile_build_http_redirect(LassoProfile *profile,
 {
 	char *query = NULL;
 	int rc = 0;
-	LassoSignatureContext context = LASSO_SIGNATURE_CONTEXT_NONE;
+	LassoSignatureContext context;
 
 	goto_cleanup_if_fail_with_rc (url != NULL, LASSO_PROFILE_ERROR_UNKNOWN_PROFILE_URL);
 	/* if message is signed, remove XML signature, add query signature */

lasso/id-ff/provider.c
@@ -1807,7 +1807,7 @@ lasso_provider_set_server_signing_key(LassoProvider *provider,
 		LassoKey *key)
 {
 	lasso_error_t rc = 0;
-	LassoSignatureContext context = LASSO_SIGNATURE_CONTEXT_NONE;
+	LassoSignatureContext context;
 
 	lasso_bad_param(PROVIDER, provider);
 	lasso_bad_param(KEY, key);

lasso/saml-2.0/login.c
@@ -731,8 +731,6 @@ lasso_saml20_login_build_assertion(LassoLogin *login,
 	goto_cleanup_if_fail_with_rc(LASSO_IS_SAMLP2_RESPONSE(profile->response),
 			LASSO_PROFILE_ERROR_MISSING_RESPONSE);
 
-	response = (LassoSamlp2Response*)profile->response;
-
 	assertion = LASSO_SAML2_ASSERTION(lasso_saml2_assertion_new());
 	assertion->ID = lasso_build_unique_id(32);
 	lasso_assign_string(assertion->Version, "2.0");

lasso/xml/xml.c
@@ -792,7 +792,7 @@ xmlNode*
 lasso_node_get_xmlNode(LassoNode *node, gboolean lasso_dump)
 {
 	xmlNode *xmlnode = NULL;
-	LassoSignatureContext context = LASSO_SIGNATURE_CONTEXT_NONE;
+	LassoSignatureContext context;
 	LassoNodeClassData *node_data;
 
 	g_return_val_if_fail (LASSO_IS_NODE(node), NULL);
@@ -1764,7 +1764,6 @@ lasso_node_impl_init_from_xml(LassoNode *node, xmlNode *xmlnode)
 					BAD_CAST LASSO_LIB_HREF, &what, LASSO_SIGNATURE_TYPE_NONE+1,
 					LASSO_SIGNATURE_TYPE_LAST))
 				break;
-			type = what;
 			private_key_password = xmlGetNsProp(xmlnode, LASSO_PRIVATE_KEY_PASSWORD_ATTRIBUTE,
 				BAD_CAST LASSO_LIB_HREF);
 			if (! private_key)

commit 875f40832b7fe58c9ce2f0ae4a0d368f4c5d32ae

Restore not-unused code
Clang was wrong on one instance, a value must be initialized to its NULL
state before using any lasso_assign_ macro with it.

Bug introduced in 4789e8d4d68eb.

lasso/saml-2.0/profile.c
@@ -1168,7 +1168,7 @@ lasso_saml20_profile_build_http_redirect(LassoProfile *profile,
 {
 	char *query = NULL;
 	int rc = 0;
-	LassoSignatureContext context;
+	LassoSignatureContext context = LASSO_SIGNATURE_CONTEXT_NONE;
 
 	goto_cleanup_if_fail_with_rc (url != NULL, LASSO_PROFILE_ERROR_UNKNOWN_PROFILE_URL);
 	/* if message is signed, remove XML signature, add query signature */
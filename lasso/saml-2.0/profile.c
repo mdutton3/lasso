875f40832b7fe58c9ce2f0ae4a0d368f4c5d32ae
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-07-30 17:17:08 +0200

    Restore not-unused code
    Clang was wrong on one instance, a value must be initialized to its NULL
    state before using any lasso_assign_ macro with it.
    
    Bug introduced in 4789e8d4d68eb.

--- lasso/saml-2.0/profile.c
+++ lasso/saml-2.0/profile.c
@@ -1168,7 +1168,7 @@ lasso_saml20_profile_build_http_redirect(LassoProfile *profile,
 {
 	char *query = NULL;
 	int rc = 0;
-	LassoSignatureContext context;
+	LassoSignatureContext context = LASSO_SIGNATURE_CONTEXT_NONE;
 
 	goto_cleanup_if_fail_with_rc (url != NULL, LASSO_PROFILE_ERROR_UNKNOWN_PROFILE_URL);
 	/* if message is signed, remove XML signature, add query signature */
