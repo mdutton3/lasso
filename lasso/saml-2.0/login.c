cad7b33c733e9b34b1bf930d6cfa7e3d7ed017eb
Frédéric Péters <fpeters@entrouvert.com>
2014-08-11 09:03:57 +0200

    saml-2.0: set NotOnOrAfter on the Conditions element (#5248)
    This is actually required for interoperability with Dropbox SAML support.

--- lasso/saml-2.0/login.c
+++ lasso/saml-2.0/login.c
@@ -744,6 +744,7 @@ lasso_saml20_login_build_assertion(LassoLogin *login,
 	assertion->Issuer = LASSO_SAML2_NAME_ID(lasso_saml2_name_id_new_with_string(
 			LASSO_PROVIDER(profile->server)->ProviderID));
 	assertion->Conditions = LASSO_SAML2_CONDITIONS(lasso_saml2_conditions_new());
+	lasso_assign_string(assertion->Conditions->NotOnOrAfter, notOnOrAfter);
 
 	audience_restriction = LASSO_SAML2_AUDIENCE_RESTRICTION(
 			lasso_saml2_audience_restriction_new());
